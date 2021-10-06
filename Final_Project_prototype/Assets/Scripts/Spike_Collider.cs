28cac8d57af2ddd74c942c78d60a7fbbaaaa4348
NirShervi <75904114+NirShervi@users.noreply.github.com>
2021-10-06 16:21:51 +0300

    create obsticale
    added asset (dagger) and creating daggers with trigger killing players

---
+++ Final_Project_prototype/Assets/Scripts/Spike_Collider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike_Collider : MonoBehaviour
+{
+    public GameObject player1, player2;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == player1.name ||
+            other.gameObject.name == player2.name)
+        {
+            Destroy(player1);
+            Destroy(player2);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
